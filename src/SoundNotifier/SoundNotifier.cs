0941c9fe89432af0c3a7cb09c1455d64692da7dc
Matt Burke <maburke@sep.com>
2010-08-05 10:43:11 -0400

    Added a sound notifier.

---
+++ src/SoundNotifier/SoundNotifier.cs
@@ -0,0 +1,18 @@
+﻿using System.Media;
+using Pomodo7o;
+
+namespace SoundNotifier
+{
+    public class SoundNotifier : BasePublisher
+    {
+        public override void RestComplete()
+        {
+            SystemSounds.Exclamation.Play();
+        }
+
+        public override void WorkComplete()
+        {
+            SystemSounds.Exclamation.Play();
+        }
+    }
+}
